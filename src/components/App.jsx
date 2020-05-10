17a071544ad59a8c6dec4a21767b48b8bf2076c1
Buchmei Ivan <buchmey.ivan@gmail.com>
2020-05-10 12:10:43 +0300

    Add a WillWatch button, design main page

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import moviesData from './moviesData';
+import MovieItem from './MovieItem';
+import './App.css';
+
+// UI = fn(statem props)
+
+class App extends React.Component {
+  constructor() {
+    super()
+      this.state = {
+        movies: moviesData,
+        moviesWillWatch: []
+      };
+  }
+
+removeMovie = movie => {
+  const updateMovies = this.state.movies.filter(function(item) {
+    return item.id !== movie.id;
+    })
+    console.log(updateMovies);
+    this.setState({
+      movies: updateMovies
+    });
+  }
+addMovieToWillWatch = movie => {
+  console.log(movie);
+  const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+  this.setState({
+    moviesWillWatch: updateMoviesWillWatch
+  });
+
+}
+
+  render() {
+    console.log(this)
+    return (
+      <div className = "container">
+        <div className = "row">
+          <div className = "col-9">
+            <div className = "row">
+              {this.state.movies.map(movie => {
+                return (
+                  <div className = "col-6" key={movie.id}>
+                  <MovieItem
+                    movie={movie}
+                    removeMovie={this.removeMovie}
+                    addMovieToWillWatch = {this.addMovieToWillWatch}
+
+                  />
+                  </div>
+                  );
+              })}
+            </div>
+          </div>
+          <div className = "col-3">
+            <p>Will Watch: {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
