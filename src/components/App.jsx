54ddc44649ed91a6e32a35fa4baf0bf3d4abecca
Buchmei Ivan <buchmey.ivan@gmail.com>
2020-05-12 00:59:01 +0300

    Add button Rem/add Will Watch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,76 @@
-import React from 'react';
-import moviesData from './moviesData';
-import MovieItem from './MovieItem';
-import './App.css';
+import React from "react";
+import moviesData from "./moviesData";
+import MovieItem from "./MovieItem";
+import "./App.css";
 
 // UI = fn(statem props)
 
 class App extends React.Component {
   constructor() {
-    super()
-      this.state = {
-        movies: moviesData,
-        moviesWillWatch: []
-      };
+    super();
+    this.state = {
+      movies: moviesData,
+      moviesWillWatch: [],
+    };
   }
 
-removeMovie = movie => {
-  const updateMovies = this.state.movies.filter(function(item) {
-    return item.id !== movie.id;
-    })
+  removeMovie = (movie) => {
+    const updateMovies = this.state.movies.filter(function (item) {
+      return item.id !== movie.id;
+    });
     console.log(updateMovies);
     this.setState({
-      movies: updateMovies
+      movies: updateMovies,
     });
-  }
+  };
 
-addMovieToWillWatch = movie => {
-  console.log(movie);
-  const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
-  this.setState({
-    moviesWillWatch: updateMoviesWillWatch
-  });
+  addMovieToWillWatch = (movie) => {
+    console.log(movie);
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch,
+    });
+  };
 
-}
+  removeMovieFromWillWatch = (movie) => {
+    const updateMoviesWillWatch = this.state.moviesWillWatch.filter(function (
+      item
+    ) {
+      return item.id !== movie.id;
+    });
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch,
+    });
+  };
 
   render() {
-    console.log(this)
+    console.log(this);
     return (
-      <div className = "container">
-        <div className = "row">
-          <div className = "col-9">
-            <div className = "row">
-              {this.state.movies.map(movie => {
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <div className="row">
+              {this.state.movies.map((movie) => {
                 return (
-                  <div className = "col-6" key={movie.id}>
-                  <MovieItem
-                    movie={movie}
-                    removeMovie={this.removeMovie}
-                    addMovieToWillWatch = {this.addMovieToWillWatch}
-
-                  />
+                  <div className="col-6" key={movie.id}>
+                    <MovieItem
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                      removeMovieFromWillWatch={this.removeMovieFromWillWatch}
+                    />
                   </div>
-                  );
+                );
               })}
             </div>
           </div>
-          <div className = "col-3">
+          <div className="col-3">
             <p>Will Watch: {this.state.moviesWillWatch.length}</p>
           </div>
         </div>
       </div>
     );
   }
-};
+}
 
 export default App;
