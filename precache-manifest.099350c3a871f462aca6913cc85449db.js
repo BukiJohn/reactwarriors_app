1b1f979b3582fa59c54c8472de213d292c901ac7
Buchmei Ivan <buchmey.ivan@gmail.com>
2020-05-18 19:50:36 +0300

    Updates

---
+++ precache-manifest.099350c3a871f462aca6913cc85449db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725497218338e76ffe721794d79283e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485dfe68521c05737369",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "75b10dd8fdae00511a81",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "485dfe68521c05737369",
+    "url": "/static/js/2.587954fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.587954fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b10dd8fdae00511a81",
+    "url": "/static/js/main.bb414d7a.chunk.js"
+  },
+  {
+    "revision": "1490fcd32cb07a7124ba",
+    "url": "/static/js/runtime-main.6af362f9.js"
+  }
+]);
