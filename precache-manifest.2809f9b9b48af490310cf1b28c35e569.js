1121767a6a13de4d80ce55b83a8faf9cc4d269d2
Buchmei Ivan <buchmey.ivan@gmail.com>
2020-05-18 20:03:46 +0300

    Updates

---
+++ precache-manifest.2809f9b9b48af490310cf1b28c35e569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d36ef333498d1107b36c370495b5d62",
+    "url": "/reactwarriors_app/index.html"
+  },
+  {
+    "revision": "485dfe68521c05737369",
+    "url": "/reactwarriors_app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "75b10dd8fdae00511a81",
+    "url": "/reactwarriors_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "485dfe68521c05737369",
+    "url": "/reactwarriors_app/static/js/2.587954fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwarriors_app/static/js/2.587954fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b10dd8fdae00511a81",
+    "url": "/reactwarriors_app/static/js/main.bb414d7a.chunk.js"
+  },
+  {
+    "revision": "fdb9896bb530d74e73fa",
+    "url": "/reactwarriors_app/static/js/runtime-main.dba70a30.js"
+  }
+]);
